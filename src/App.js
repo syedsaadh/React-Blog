a95a179aca71c1d99b2a959023444790eba6adf3
Syed Saad <hassan.saad.mail@gmail.com>
2017-07-08 02:07:08 +0530

    Route for Specific Article

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './App.css';
 import AppNavbar from './components/AppNavbar';
 import WritePost from "./pages/WritePost";
 import Blog from "./pages/Blog";
+import Article from "./pages/Article"
 
 class App extends React.Component {
   render() {
@@ -20,6 +21,7 @@ class App extends React.Component {
             <Route path="/" />
             <Route path="/writepost" component={WritePost} />
             <Route path="/blog" component={Blog} />
+            <Route path="/article/:id" component={Article}/>
             </div>
           </Router>
       </div>
